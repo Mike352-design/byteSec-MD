d7f045f2ff8d6373361f2c067ba1a41208720954
Henry <henry.arcangello@proton.me>
2024-11-02 01:50:06 +0000

    update plugins/dono-update.js

--- plugins/dono-update.js
+++ plugins/dono-update.js
@@ -13,7 +13,7 @@ if (global.conn.user.jid == conn.user.jid) {
 let stdout = execSync('git pull origin master' + (m.fromMe && text ? ' ' + text : ''))
 conn.reply(m.chat, stdout.toString(), m)}
 //} catch {
-var update = execSync('git remote set-url origin https://github.com/ARCANGEL0/Edgar-WhatsappBOT.git && git pull origin master -f')
+var update = execSync('git remote set-url origin https://github.com/ARCANGEL0/bytesec-md.git && git pull origin master -f')
 await m.reply(update.toString())
 } catch {
 await m.reply(`${fg}`) 
