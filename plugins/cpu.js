8cdbe015482e263a2b97f4a86f64c89e51ba3c09
Henry <henry.arcangello@proton.me>
2024-11-06 21:55:27 +0000

    update plugins/cpu.js

--- plugins/cpu.js
+++ plugins/cpu.js
@@ -38,7 +38,7 @@ let _muptime
 if (process.send) { process.send('uptime')
 _muptime = await new Promise(resolve => { process.once('message', resolve) 
 setTimeout(resolve, 2000) }) * 1000}
-let uptime = '> ' + clockString(_uptime)
+let uptime = '> ' + clockString(_uptime, global.db.data.chats[m.chat].language)
     try {
       
   //await m.reply('_Realizando test_')
@@ -278,13 +278,13 @@ handler.command = /^(cpu|status|infogrupo|infochats)$/i
 
 export default handler
 
-function clockString(ms) {
+function clockString(ms, lang) {
     let d = isNaN(ms) ? '--' : Math.floor(ms / 86400000);
     let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24;
     let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60;
     let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60;
 
-    if (global.db.data.chats[m.chat].language === 'pt') {
+    if (lang === 'pt') {
         return [d, ' *Dias* ', h, ' *Horas,* ', m, ' *Minutos* *e* ', s, ' *Segundos* ']
             .map(v => v.toString().padStart(2, '0')).join('');
     } else {
