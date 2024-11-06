13f61811ed3c315a21daec2522f87a2cea46ae53
Henry <henry.arcangello@proton.me>
2024-11-06 10:17:48 +0000

    update plugins/cpu.js

--- plugins/cpu.js
+++ plugins/cpu.js
@@ -264,7 +264,7 @@ function clockString(ms) {
     let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24;
     let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60;
     let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60;
-    return [' ' + d, ' *Dias* ', h, ' *Horas,* ', m, ' *Minutos*\n┃   *e* ', s, ' *Segundos* '].map(v => v.toString().padStart(2, 0)).join('');
+    return [d, ' *Dias* ', h, ' *Horas,* ', m, ' *Minutos* *e* ', s, ' *Segundos* '].map(v => v.toString().padStart(2, 0)).join('');
 }
 
 
