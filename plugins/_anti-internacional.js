7e3f9d0ce7a1b54224d9e6c729e310b73150016e
Henry <henry.arcangello@proton.me>
2024-11-07 09:56:11 +0000

    update plugins/_anti-internacional.js

--- plugins/_anti-internacional.js
+++ plugins/_anti-internacional.js
@@ -116,7 +116,7 @@ if (global.db.data.chats[m.chat].language === 'pt') {
 const fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net"
 }
 if (!m.isGroup) return !1
-if (!m.fromMe) return !1
+if (m.fromMe) return !1
  
 
 
