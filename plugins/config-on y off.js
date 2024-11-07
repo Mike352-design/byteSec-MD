0010f7b73540c8056cfd17fb59aaef7a92edf4a7
Henry <henry.arcangello@proton.me>
2024-11-07 17:21:49 +0000

    update plugins/config-on y off.js

--- plugins/config-on y off.js
+++ plugins/config-on y off.js
@@ -1013,9 +1013,9 @@ return await m.reply(`┏━─────────────━┓
 throw false
 }
 
-let modoGrupo = global.db.data.chats[m.chat].language === 'pt' ? `0x0041F SISTEMA/GRUPO` : `0x0041F SYSTEM/GROUP`
+let modoGrupo = global.db.data.chats[m.chat].language === 'pt' ? ` SISTEMA/GRUPO` : ` SYSTEM/GROUP`
 
-let modoBot = global.db.data.chats[m.chat].language === 'pt' ? '0x0041F SISTEMA/TODOS' : '0x0041F SYSTEM/ALL'
+let modoBot = global.db.data.chats[m.chat].language === 'pt' ? ' SISTEMA/TODOS' : ' SYSTEM/ALL'
   
 let resultado = `root@bytesec: #~ cfs --${command + ' ' + text}
 ⎓────────
